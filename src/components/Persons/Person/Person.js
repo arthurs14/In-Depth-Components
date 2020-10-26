57a5eac35ff279b4415a92bd4b0cfb2f31edb6f5
Arthur Sandro <arthurcs426@gmail.com>
2020-10-26 16:10:38 -0700

    conversion of person and persons component to class

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import classes from './Person.module.css';
 
-const Person = ({ click, name, age, changed }) => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={click}>
-        {`I'm ${name} and I am ${age} years old!`}
-      </p>
-      <input type="text" onChange={changed} value={name} />
-    </div>
-  );
-};
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...')
+    const { click, name, age, changed } = this.props;
+    return (
+      <div className={classes.Person}>
+        <p onClick={click}>
+          {`I'm ${name} and I am ${age} years old!`}
+        </p>
+        <input type="text" onChange={changed} value={name} />
+      </div>
+    );
+  }
+}
 
 export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const Persons = ({ persons, click, changed }) => {
-  console.log('[Persons.js] rendering...')
-  return persons.map((person, index) => {
-    return (<Person
-      click={() => click(index)}
-      name={person.name}
-      age={person.age}
-      key={index}
-      changed={(event) => changed(event, person.id)}
-    />
-    );
-  });
-};
+class Persons extends Component {
+  render() {
+    console.log('[Persons.js] rendering...');
+    const { persons, click, changed } = this.props;
+
+    return persons.map((person, index) => {
+      return (<Person
+        click={() => click(index)}
+        name={person.name}
+        age={person.age}
+        key={index}
+        changed={(event) => changed(event, person.id)}
+      />
+      );
+    });
+  }
+}
 
 export default Persons;
