332d050ce0a3d4487caff98e880abd0e6ac43851
Arthur Sandro <arthurcs426@gmail.com>
2020-10-28 15:37:31 -0700

    the proper way to work with old state when using setState in class components

--- src/containers/App.js
+++ src/containers/App.js
@@ -53,7 +53,13 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    // update state when depending on old state
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1,
+      };
+    });
   }
 
   deletePersonHandler = (personIndex) => {
